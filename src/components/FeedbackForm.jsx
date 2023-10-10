d8866c99ddc216b79f7c372586c9762dff47502b
Alex Bilba <alex.shanless@gmail.com>
2023-10-09 19:17:03 -0700

    edit button

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -2,14 +2,22 @@ import FeedbackContext from '../context/FeedbackContext';
 import RatingSelect from './RatingSelect';
 import Button from './shared/Button';
 import Card from './shared/Card';
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
 
 function FeedbackForm() {
-  const [text, SetText] = useState('');
-  const [rating, SetRating] = useState(10);
+  const [text, setText] = useState('');
+  const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState('');
-  const { addFeedback } = useContext(FeedbackContext);
+  const { addFeedback, feedbackEdit } = useContext(FeedbackContext);
+
+  useEffect(() => {
+    if (feedbackEdit.edit === true) {
+      setBtnDisabled(false);
+      setText(feedbackEdit.item.text);
+      setRating(feedbackEdit.item.rating);
+    }
+  }, [feedbackEdit]);
 
   const handleTextChange = e => {
     if (text === '') {
@@ -22,7 +30,7 @@ function FeedbackForm() {
       setMessage(null);
       setBtnDisabled(false);
     }
-    SetText(e.target.value);
+    setText(e.target.value);
   };
 
   const handleSubmit = e => {
@@ -33,15 +41,14 @@ function FeedbackForm() {
         rating,
       };
       addFeedback(newFeedback);
-
-      SetText('');
+      setText('');
     }
   };
   return (
     <Card>
       <form onSubmit={handleSubmit}>
         <h2>How would rate your service with us?</h2>
-        <RatingSelect select={rating => SetRating(rating)} />
+        <RatingSelect select={rating => setRating(rating)} />
         <div className='input-group'>
           <input
             onChange={handleTextChange}
